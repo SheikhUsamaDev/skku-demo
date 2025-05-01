291b6e40b9ec6f52c873dcd40844cedd2a28c88b
usama <usamasaeed4381@gmail.com>
2025-05-02 03:30:15 +0500

    test

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -174,7 +174,7 @@ const [currentIndex, setCurrentIndex] = useState(0);
 useEffect(() => {
   const interval = setInterval(() => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % cards.length);
-  }, 31000);
+  }, 3000);
 
   return () => clearInterval(interval);
 }, []);
